95a36833963c070816f670a420d6af4fb84667ea
Kenneth Chua <cloudberry2011@gmail.com>
2024-06-13 14:40:03 +0800

    create the data record form and the record info form

--- data_entry_app.py
+++ data_entry_app.py
@@ -12,5 +12,15 @@ ttk.Label(
         font=('TkDefaultFont', 16)
         ).grid()
 
+# Build the data record form in a Frame
+drf = ttk.Frame(root)
+drf.grid(padx=10, sticky=(tk.E + tk.W))
+drf.columnconfigure(0, weight=1)
+
+r_info = ttk.LabelFrame(drf, text='Record Information')
+r_info.grid(sticky=(tk.W + tk.E))
+for i in range(3):
+    r_info.columnconfigure(i, weight=1)
+
 # Execute mainloop
 root.mainloop()
