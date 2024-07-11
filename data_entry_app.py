35180de9bc31fdff8a9f0f2781c58f491e4699ef
Kenneth Chua <cloudberry2011@gmail.com>
2024-07-12 04:50:33 +0800

    Create the medium field

--- data_entry_app.py
+++ data_entry_app.py
@@ -152,6 +152,12 @@ ttk.Spinbox(
         ).grid(row=3, column=1, sticky=(tk.W + tk.E))
 
 # Med Height
+variables['Med Height'] = tk.DoubleVar()
+ttk.Label(p_info, text='Med Height (cm)').grid(row=2, column=2)
+ttk.Spinbox(
+        p_info, textvariable=variables['Med Height'],
+        from_=0, to=1000, increment=0.01
+        ).grid(row=3, column=2, sticky=(tk.W + tk.E))
 
 # Execute mainloop
 root.mainloop()
